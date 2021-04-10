38d6d0f83666ac9a08b2d93478dd16c1a788d90f
Sam Dillard <sam@influxdata.com>
2021-04-10 14:49:53 -0700

    small fix -- now timestamps are repeating themselves per line -- need to fix

--- generator/line.py
+++ generator/line.py
@@ -21,20 +21,44 @@ class Line:
         return f"{self.measurement}{self.tagset} {self.fieldset} {self.timestamp}" 
 
     @classmethod
-    def gen_line(measurement=None, tags=None, tagset=None, fields=None, fieldset=None, **kwargs):
+    def gen_line(cls, measurement=None, timestamp=None, tagset=None, fieldset=None, tags=None, fields=None, **kwargs):
         
+        if not timestamp:
+            timestamp = Timestamp(precision=kwargs["time_precision"])
         if fieldset and tagset:
             line = Line(measurement=measurement, tagset=Tagset(**kwargs),
-                        fieldset=Fieldset(**kwargs), timestamp=time_precision)
-            return line
+                        fieldset=Fieldset(**kwargs), timestamp=timestamp)
+            return cls(line)
         elif tags and fields:
             tagset = Tagset(tags)
             fieldset = Fieldset(fields)
             line = Line(measurement=measurement, tagset=Tagset(**kwargs),
-                        fieldset=Fieldset(**kwargs), timestamp=time_precision)
-            return line
+                        fieldset=Fieldset(**kwargs), timestamp=timestamp)
+            return cls(line)
         else:
-            return Line()
+            if kwargs["num_tags"]:
+                tset = Tagset()
+                for t in range(kwargs["num_tags"]):
+                    tset.add_tag(key_len=kwargs["tag_key_length"], val_len=kwargs["tag_value_length"])
+
+            fset = Fieldset()
+
+            if kwargs["int_fields"]:
+                for i in range(kwargs["int_fields"]):
+                    fset.add_int(key_len=kwargs["field_key_length"], val_len=kwargs["int_value_length"])
+            if kwargs["float_fields"]:
+                for i in range(kwargs["float_fields"]):
+                    fset.add_float(key_len=kwargs["field_key_length"], val_len=kwargs["float_value_length"])
+            if kwargs["str_fields"]:
+                for i in range(kwargs["str_fields"]):
+                    fset.add_str(key_len=kwargs["field_key_length"], val_len=kwargs["str_value_length"])
+            if kwargs["bool_fields"]:
+                for i in range(kwargs["bool_fields"]):
+                    fset.add_bool(key_len=kwargs["field_key_length"], val_len=kwargs["bool_value_length"])
+            
+            line = Line(measurement=measurement, tagset=tset,
+                        fieldset=fset, timestamp=timestamp)
+            return cls(line)
 
 
 
